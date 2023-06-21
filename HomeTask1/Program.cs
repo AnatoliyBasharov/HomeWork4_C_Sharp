43edc8777ac9a7fd1a339894df2221ce7a03be44
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-21 22:30:06 +0300

    Finish to work HomeTask1

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int numberOne = ReadInt("Введите число ");
+int numberTwo = ReadInt("Введите второе число ");
+int sum = 1;
+int temp = numberOne;
+while (numberTwo > sum)
+{
+    temp *= numberOne;
+    sum++;
+}
+System.Console.WriteLine(temp);
