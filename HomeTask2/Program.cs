187f6ab47b35fb5f8f612f88aa2b1f79ec48c135
Anatoliy Basharov <rexonka3@gmail.com>
2023-06-25 10:57:05 +0300

    I  dont work homeTask2

--- HomeTask2/Program.cs
+++ HomeTask2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int number = ReadInt("Введите число ");
+Char[] array = number.ToString().ToCharArray();
+int index = 0;
+int sum = 0;
+while (index < array.Length-1)
+{
+    sum = sum+ array[0];
+    index++;
+    System.Console.Write(sum);
+}
